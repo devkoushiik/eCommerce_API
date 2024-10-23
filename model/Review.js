af6625549f2d647ad8cef9027d21f8b3b46917a6
koushikxahmed <koushikxahmed@gmail.com>
2024-10-23 17:39:38 +0600

    review model added

---
+++ model/Review.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please provide rating"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide review title"],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, "Please provide review text"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model("Review", reviewSchema);
